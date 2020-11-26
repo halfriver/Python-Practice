732ddeac57cd7e7b7386791ab08aad0199d61e56
halfriver <ohkawa2.0@gmail.com>
2020-11-26 01:03:31 -0500

    problems 9 and 10

--- Problem 11 - Multiplication Table.py
+++ Problem 11 - Multiplication Table.py
@@ -5,26 +5,35 @@
 # Original: 12 Oct 2019
 # Edited: 25 November 2020
 
-
-n = int(input("Give me a number and I'll make a multiplication table with that many rows and columns. \n"))
-
 def table(n):
     table = [[" "]]
-    inc = 1
-    # Fill in first row (unique)
-    for x in range(1,n+1):
+    # fill in first row (unique)
+    for x in range(1, n+1):
         table[0].append(str(x))
-    # Add and populate new rows
-    for y in range(1,n+1):
+    # add and populate new rows
+    for y in range(1, n+1):
         table.append([str(y)])
-        for z in range(1,n+1):
+        for z in range(1, n+1):
             table[y].append(str(y*z))
+    # add spaces so that the table is consistent 
     space = len(table[n][n])
     for row in range(n+1):
         for cell in range(n+1):
             while len(table[row][cell]) < space:
                 table[row][cell] = " " + table[row][cell]
-    return(table)
+    return table
+
+# error testing for input
+while True:
+    try:
+        n = int(input("Give me a number and I'll make a multiplication table with that many rows and columns.\n").replace(" ", ""))
+        if n < 0:
+            print("You must give a positive number. Try again.")
+            continue
+        break
+    except ValueError:
+        print("That's not a valid number. Try again.")
+    
 
 for row in table(n):
     print(row)
