1e855205956ae8dab65814e2bd1faab4e51d3127
halfriver <ohkawa2.0@gmail.com>
2021-01-05 00:09:50 -0500

    add date, create next project files

---
+++ my solutions/Problem 35 - 2048 Game.py
@@ -0,0 +1,9 @@
+'''
+Problem 35 - 2048 Game
+- The 2048 game is a single-player game in which the user has to slide the tiles and the tiles with the same number will merge into a single tile with the sum of their two values.
+- Create a program that allows the user to play this game.
+- The game ends when all the tiles are filled and no more combinations can be made.
+'''
+
+# Original: 05 January 2021
+
